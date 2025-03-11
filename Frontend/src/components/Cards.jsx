e7b015edb5b5e91a3133e94cfa4aca175b7dae58
chanchal goyal <miss71910@gmail.com>
2025-03-11 23:16:00 +0530

    naming convention

--- Frontend/src/components/Cards.jsx
+++ Frontend/src/components/Cards.jsx
@@ -7,7 +7,7 @@ function Cards({ item }) {
       <div className="mt-4 my-3 p-3">
         <div className="card bg-base-100 w-92 shadow-xl hover:scale-105 duration-200 dark:bg-slate-900 dark:text-white dark:border">
           <figure>
-            <img src={item.image} alt="Shoes" />
+            <img src={item.image} alt="books" />
           </figure>
           <div className="card-body">
             <h2 className="card-title">
